d80707ddb8788bd382a13b5d7c1170c75fd41230
momodiagne <mdmamadou0509@gmail.com>
2023-03-20 00:25:06 +0000

    update

--- append.c
+++ append.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-char* append(char *a,char *b){
-	char* result=(char*)malloc((strlen(a)+strlen(b))*sizeof(char));
-	strcpy(result,a);
-	strcat(result,b);
-	return result;
+typedef struct{
+	int len;
+	int* ptrarr;
+
+}list;
+void append(list *l, int n){
+	int* newlist=malloc(sizeof(int)*1);
+	if (l->len==0)
+		*newlist=realloc(newlist,sizeof(int)*(l->len+1)); 
+	if(l->len & (l->len-1) == 0)
+		*newlist=realloc(newlist,sizeof(int)*(l->len*2)); 
+	for(int i=0;i<l->len;i++)
+		newlist[i]=l->ptrarr[i];
+	if(l->ptrarr!=0)
+		free(l->ptrarr);
+	l->ptrarr=newlist;
+	l->ptrarr[l->len]=n;
+	l->len+=1;
 }
-int main(){
+int main()
+{
+	list l;
+	l.len=0;
+	l.ptrarr=0;
+	for(int i=0;i<5;i++)
+               append(&l,i);
+	for(int i=0;i<5;i++)
+		printf("%d",l.ptrarr[i]);
 	return 0;
 }
+// J'ai voulu poser la condition que la longueur est un multiple de 2 par exemple 4 ou 8 ... d'allouer directement len*2 mais ça ne marche pas
